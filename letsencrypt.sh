5815c7b0d95877572667c15b25d69ee589c6f5f0
teberl <mail@teberl.de>
2019-02-27 20:50:39 +0100

    Improve letsencrypt script to support renewal

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -8,6 +8,9 @@ function letsencrypt {
         certonly --standalone --agree-tos -d $DOMAIN
 }
 
+echo -n "Renew certificate (Y/n): "
+read IS_RENEWAL
+
 # PORTS FROM docker service are still used
 # docker stack rm phx_client
 # docker stack deploy -c /etc/teberl/docker-compose.yml phx_client
@@ -16,9 +19,17 @@ function letsencrypt {
 ufw allow 80
 ufw allow 443
 
-# recreate the certificates
-letsencrypt teberl.de
+if [ "$IS_RENEWAL" == "n" ]; then
+        echo "create certificate a new certificate"
+        letsencrypt teberl.de
+else
+        echo "renew all available certificates"
+        # as of version 0.10.0, Certbot supports a renew action 
+        # to check all installed certificates for impending expiry and 
+        # attempt to renew them. The simplest form is simply
+        certbot renew
+fi
 
 # put the firewall up again
 ufw deny 80
-ufw deny 443
+ufw deny 443 
