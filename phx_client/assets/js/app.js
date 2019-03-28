2500aa981ba9f2fd28cb3a8959e2becd6989aeb6
teberl <mail@teberl.de>
2019-03-28 08:09:19 +0100

    Use a baseUrl for the hidden menu to better dev experience

--- phx_client/assets/js/app.js
+++ phx_client/assets/js/app.js
@@ -8,8 +8,10 @@ lazySizes.init();
 
 import css from '../css/app.scss';
 
+const baseUrl = location.origin;
+
 console.info('%cHey there, good to see you!', 'font-weight: bold;');
 console.info('Navigation:');
-console.info(`• https://teberl.de/todos → A phoenix_live_view TodoMVC example ${'\n'}
-• https://teberl.de/counter → The legendary phoenix_live_view Counter${'\n'}
-• https://teberl.de/heartbeat → My first route and websocket playground with phoenix${'\n'}`);
+console.info(`• ${baseUrl}/todos → A phoenix_live_view TodoMVC example ${'\n'}
+• ${baseUrl}/counter → The legendary phoenix_live_view Counter${'\n'}
+• ${baseUrl}/heartbeat → My first route and websocket playground with phoenix${'\n'}`);
